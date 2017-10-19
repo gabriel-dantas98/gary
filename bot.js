fc7ffac7ff42f2bcb69c4ab941ae9ae06e1649af
gabriel-dantas98 <gabriel.dantas98@outlook.com.br>
2017-10-19 16:29:22 -0200

    bot.js shell execute

--- bot.js
+++ bot.js
@@ -1,4 +1,6 @@
 var Botkit = require("/usr/local/lib/node_modules/botkit");
+var fs = require('fs');
+var WebClient = require('/usr/local/lib/node_modules/@slack/client').WebClient;
 const publicIp = require('/usr/local/lib/node_modules/public-ip');
 
 var controller = Botkit.slackbot({
@@ -6,105 +8,18 @@ var controller = Botkit.slackbot({
 });
 
 var bot = controller.spawn({
-
-	token: 'xoxb-191251446178-UuxzHKNPFPKMUEnfWHLhQgP9'
-
+    
+    token: 'xoxb-191251446178-cbOvvCgZrvNuoiv2iSq839k7'
+    
 }).startRTM();
 
-
-
-controller.hears(['listaip','listIp','ubuntuzera','ubuntuzeraServer','servidores', 'ubuntuip', 'meuip'], 'direct_message,direct_mention,mention', function(bot, message) {
-
-    bot.reply(message,"Lista de Ip's dos servidores");
-
+controller.hears(['public-ip'], 'direct_message, direct_mention, mention', function(bot, message){
     publicIp.v4().then(ip => {
-        console.log(ip);
-        //=> '46.5.21.123'
-        var helloText = "*Ubuntuzera* é esse aqui lek ->" + ip;
-        bot.reply(message, helloText);
-    });   
-    
-});
-
-controller.hears(['hello', 'eae', 'ta vivo?', 'morreu?', 'ta vivo'], 'direct_message,direct_mention,mention', function(bot, message) {
-    var helloText = "Meooowwww!!!";
-
-    bot.reply(message, helloText);
-});
-
-controller.hears(['instant idea', 'instant', 'algoritmo instantaneo', 'plow'], 'direct_message,direct_mention,mention', function(bot, message) {
-    var helloText = "*instant algorithm ALERT!!!*";
-
-    bot.reply(message, helloText);
-});
-
-controller.hears(['Paulo', 'Gabriel'], 'direct_message,direct_mention,mention', function(bot, message) {
-    var helloText = "*VTNC slackbot -.-'*";
-
-    bot.reply(message, helloText);
-});
-
-controller.hears(['randomNumber'], 'direct_message,direct_mention,mention', function(bot,message){
-    
-    for(var i = 0; i < 10; i++){
-        var helloText = "Number = " + Math.floor((Math.random() * i) + 1);
-        bot.reply(message, helloText);
-    }
-    
-})
-
-controller.hears(['for real'], 'direct_message,direct_mention,mention', function(bot,message){
-    
-    var scream = "*GO TO FOR REAL!!! PORRAAA!!*";
-
-    bot.reply(message,scream);
-    
-})
-
-controller.hears(['ovo', 'Ovo'], 'direct_message,direct_mention,mention', function(bot, message) {
-    var helloText = "*MIAAAAAAAAAAAAAAAAAAAAU !!1!!1onze!!11*";
-
-   bot.reply(message, helloText);
-})
-
-controller.hears(['kawaii', 'Kawaii', 'kawaii desu', 'Kawaii desu'], 'direct_message,direct_mention,mention', function(bot, message) {
-    var helloText = "*Nyan nyan nyan nyan !!!*";
-
-   bot.reply(message, helloText);
-})
-
-
-controller.hears(['gerdau', 'Gerdau', 'estagio', 'gg izi'], 'direct_message,direct_mention,mention', function(bot, message) {
-    var helloText = "*Só vem Gerdauuu!!!!*";
-    var scream = "*GO TO MINERCOIN BITCHHHH!!!*";
-    var helloText1 = "*SÒ VEM FABIOOOOOOO!!! OPEN THE $#%$%@*";
-   
-   bot.reply(message,scream);
-   bot.reply(message,helloText1); 
-   bot.reply(message, helloText);
-})
-
-
-controller.hears(['gerdau', 'Gerdau', 'estagio', 'gg izi'], 'direct_message,direct_mention,mention', function(bot, message) {
-    var helloText = "*Só vem Gerdauuu!!!!*";
-    var scream = "*GO TO MINERCOIN BITCHHHH!!!*";
-    var helloText1 = "*SÒ VEM FABIOOOOOOO!!! OPEN THE $#%$%@*";
-   
-   bot.reply(message,scream);
-   bot.reply(message,helloText1); 
-   bot.reply(message, helloText);
-})
-
-controller.hears(['gerdau', 'Gerdau', 'estagio', 'gg izi'], 'direct_message,direct_mention,mention', function(bot, message) {
-    var helloText = "*Só vem Gerdauuu!!!!*";
-    var helloText1 = "*SÒ VEM FABIOOOOOOO!!! OPEN THE $#%$%@*";
-
-   bot.reply(message,helloText1); 
-   bot.reply(message, helloText);
+       bot.reply(message, ip);
+    });    
 })
 
 controller.hears(['boruto insight', 'yura yura', 'fezinho'], 'direct_message,direct_mention,mention', function(bot, message) {
-    var boruto = "https://www.facebook.com/Otakando111/videos/vb.714255878623096/1325485490833462/?type=2&theater&notif_t=mentions_reply&notif_id=1496017737429027";
     var youtubeBoruto = "https://www.youtube.com/watch?v=9iL_QkzNQ94";
     for(var i = 0; i < 5; i++){
         bot.reply(message, youtubeBoruto);
@@ -112,31 +27,36 @@ controller.hears(['boruto insight', 'yura yura', 'fezinho'], 'direct_message,dir
      
 })
 
-controller.hears(['password','passwordList', 'pass', 'senhas'], 'direct_message,direct_mention,mention', function(bot, message) {
-    var helloText = "*Udemy - loed_0@hotmail.com | forreal767020*";
-     
-   bot.reply(message, helloText);
-})
+controller.hears(['build-upload'], 'direct_message,direct_mention,mention', function(bot, message) {
+    
+    const exec = require('child_process').exec;
+    var yourscript = exec('sh build-start.sh',
+            (error, stdout, stderr) => {
+                console.log(`${stdout}`);
+                console.log(`${stderr}`);
+                if (error !== null) {
+                    console.log(`exec error: ${error}`);
+                }
+            });
+    
+    var token = 'xoxp-190584162307-191481662663-238070906900-e03211abbb2f9987a5c9d80b7c7c5e94';
 
+    bot.reply(message,'Uploading...');
 
-controller.hears(['lista magrelin'], 'direct_message,direct_mention,mention', function(bot, message) {
-    var helloText = "https://docs.google.com/document/d/13ACcHll8BGDArQXb9pjFNbtUn_Xls_WYVCqgF11HNms/edit";
-     
-   bot.reply(message, helloText);
-})
+    var web = new WebClient(token);
+    var filePath = '/home/gabs/Documentos/upload/publicIp.py';
+    var fileName = 'publicIp.py';
 
-
-controller.hears(['lista magrelin'], 'direct_message,direct_mention,mention', function(bot, message) {
-    var anaozinCreed = "https://youtu.be/sPAeJATIyYU";
-
-    bot.reply(message, "*Tyrion Last Breath!!!*")
-
-    for(var i = 0; i < 6; i++){
-    bot.reply(message, anaozinCreed);
+    var streamOpts = {
+        file: fs.createReadStream(filePath),
+        channels: ['zettamemes']
     };
+    
+    web.files.upload(fileName, streamOpts, function handleStreamFileUpload    (err, res) {
+       console.log(res);
+    });
+     
 })
-
-
 
 
 
